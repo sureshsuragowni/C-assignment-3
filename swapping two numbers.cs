a0ae3fd71aeb7198e33f7d833c672e9d69bf39d2
sureshsuragowni <sureshgowd08@gmail.com>
2022-04-13 21:44:10 +0530

    commitrd

---
+++ swapping two numbers.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, temp;
+            Console.WriteLine("\n type 1st number : ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n type 2nd number : ");
+            number2 = int.Parse(Console.ReadLine());
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.WriteLine("\n After swapping :");
+            Console.WriteLine("\n 1st number :");
+            Console.WriteLine(number1);
+            //Console.ReadLine();
+            Console.WriteLine("\n 2nd number :");
+            Console.WriteLine(number2);
+            //Console.ReadLine();
+        }
+    }
+}
