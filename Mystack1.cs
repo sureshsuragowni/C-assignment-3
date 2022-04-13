58a8b56d3d7227a0bf11d12aa8ee5c3e170afd6f
sureshsuragowni <sureshgowd08@gmail.com>
2022-04-13 21:48:45 +0530

    commited

---
+++ Mystack1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace StackImplementation
+{
+    internal class MyStack2
+    {
+        static readonly int MAX = 1000;
+        int top;
+        int[] stack = new int[MAX];
+
+        bool IsEmpty()
+        {
+            return (top < 0);
+        }
+        public MyStack2()
+        {
+            top = -1;
+        }
+        internal bool Push(int data)
+        {
+            if (top >= MAX)
+            {
+                Console.WriteLine("Stack Overflow");
+                return false;
+            }
+            else
+            {
+                stack[++top] = data;
+                return true;
+            }
+        }
+
+        internal int Pop()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return 0;
+            }
+            else
+            {
+                int value = stack[top--];
+                return value;
+            }
+        }
+
+        internal void Peek()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+            else
+                Console.WriteLine("The topmost element of Stack is : {0}", stack[top]);
+        }
+
+        internal void PrintStack()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Items in the Stack are :");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack2 myStack = new MyStack2();
+
+            myStack.Push(15);
+            myStack.Push(27);
+            myStack.Push(54);
+            myStack.Push(69);
+            myStack.PrintStack();
+            myStack.Peek();
+            Console.WriteLine("Item popped from Stack : {0}", myStack.Pop());
+            myStack.PrintStack();
+        }
+    }
+}
